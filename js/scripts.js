f42e5b57221c170aba85331f78e4fb6c97503bbb
michalvapeshop-ai <michal.vapeshop@gmail.com>
2026-01-09 17:32:43 +0000

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+let cart = [];
+
+function addToCart(productName, price) {
+    cart.push({ productName, price });
+    console.log('Produkty w koszyku: ', cart);
+}
